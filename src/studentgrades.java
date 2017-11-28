05e89a89fb38d79009cbb982e7161e2aa5b15fd4
JRSock <riley.sockwell@gmail.com>
2017-11-28 09:44:17 -0800

    Initial Commit

---
+++ src/studentgrades.java
@@ -0,0 +1,68 @@
+public class studentgrades {
+    public static void main(String[] args){
+        final int num_students = 100;
+
+        boolean[] failing = new boolean[num_students];
+        boolean[] passing = new boolean[num_students];
+        int[] score = new int[num_students];
+
+        int counter = 0;
+
+        while(counter < failing.length){
+            failing[counter] = false;
+            counter++;
+        }
+
+        counter = 0;
+
+        while(counter < score.length){
+            score[counter] = (int)(Math.random() * 101);
+            System.out.println("Score " + (counter + 1) + ": " + score[counter]);
+            counter++;
+        }
+
+        counter = 0;
+
+        while(counter < num_students){
+            if(score[counter] < 60){
+                failing[counter] = true;
+            }
+            else{
+                passing[counter] = true;
+            }
+            counter++;
+        }
+
+        counter = 0;
+        int holder = 0;
+
+        while(counter < 50){
+            holder = score[counter];
+            score[counter] = score[99-counter];
+            score[99-counter] = holder;
+            counter++;
+        }
+
+        counter = 0;
+        int largest = 0;
+
+        while(counter < num_students) {
+            holder = score[counter];
+            largest = Math.max(holder, largest);
+            counter++;
+        }
+
+        System.out.println();
+        System.out.println("Largest Score: " + largest);
+        int sum = 0;
+        counter = 0;
+
+        while(counter < num_students){
+            sum = score[counter] + sum;
+            counter++;
+        }
+        System.out.println("Sum of Scores: " + sum);
+
+        System.out.println("Average of Scores: " + (sum/100));
+    }
+}
