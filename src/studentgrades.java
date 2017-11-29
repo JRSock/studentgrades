97cf45fe19cee676dc7e7c65fd74316f452b0b21
JRSock <riley.sockwell@gmail.com>
2017-11-29 14:04:38 -0800

    Adds file reading

--- src/studentgrades.java
+++ src/studentgrades.java
@@ -1,24 +1,33 @@
+import java.util.Scanner;
+
 public class studentgrades {
     public static void main(String[] args){
-        final int num_students = 100;
+        int num_students = 0;
+        Scanner sc1 = new Scanner(System.in);
+
+        System.out.print("How many scores would you like to calculate: ");
+        num_students = sc1.nextInt();
 
         boolean[] failing = new boolean[num_students];
         boolean[] passing = new boolean[num_students];
         int[] score = new int[num_students];
         int[] freq = new int[num_students + 1];
+        String[] names = new String[num_students];
 
         int counter = 0;
+        Scanner sc2 = new Scanner("scores.dat");
 
-        while(counter < failing.length){
-            failing[counter] = false;
+        while(counter < score.length){
+            score[counter] = sc2.nextInt();
             counter++;
         }
 
         counter = 0;
+        Scanner sc3 = new Scanner("names.dat");
 
         while(counter < score.length){
-            score[counter] = (int)(Math.random() * 101);
-            System.out.println("Score " + (counter + 1) + ": " + score[counter]);
+            names[counter] = sc3.nextLine();
+            System.out.println(names[counter] + " got a " + score[counter]);
             counter++;
         }
 
@@ -46,15 +55,19 @@ public class studentgrades {
 
         counter = 0;
         int largest = 0;
+        int largestpos = 0;
 
         while(counter < num_students) {
             holder = score[counter];
             largest = Math.max(holder, largest);
+            if(largest == holder){
+                largestpos = counter;
+            }
             counter++;
         }
 
         System.out.println();
-        System.out.println("Largest Score: " + largest);
+        System.out.println(names[largestpos] + " had the highest score in the class with a " + score[largestpos]);
         int sum = 0;
         counter = 0;
 
@@ -70,6 +83,19 @@ public class studentgrades {
             counter++;
         }
 
+        counter = 0;
+        largest = 0;
+        int freqpos = 0;
+
+        while(counter < num_students){
+            holder = freq[counter];
+            largest = Math.max(holder, largest);
+            if(largest == holder){
+                freqpos = counter;
+            }
+            counter++;
+        }
         System.out.println("Average of Scores: " + (sum/100));
+        System.out.println("Most Frequent Score: " + score[freqpos]);
     }
 }
